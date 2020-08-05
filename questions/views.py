92a3e84f81f5d8cd363bb1ffda31392261e615f0
AnjalBam <anjalbam81@gmail.com>
2020-08-05 19:41:52 +0545

    [Addded] Pagination added to the list view to get 1 result

--- questions/views.py
+++ questions/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 # from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, CreateAPIView
 from .seriralizers import QuestionSerializer, Question
+from .pagination import MyPageNumberPagination
 
 
 class ListAllQuestionsAPI(ListAPIView):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
+    pagination_class = MyPageNumberPagination
 
 
 class CreateQuestionAPI(CreateAPIView):
